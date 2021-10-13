da2c4e0c9d6de9c8fa0af942fd23eb938b1d7eef
Joe McGrath <joseph.mcgrath@techworks.ie>
2021-10-13 12:32:39 +0100

    init commit

--- part_2.py
+++ part_2.py
@@ -8,28 +8,44 @@ import skimage as sk
 if __name__ == '__main__':
     picture = np.array(Image.open("/home/joe/PycharmProjects/Image-Processing/scissors_col_2.jpg"))
 
-    fig1, axes1 = plt.subplots(1, 3, figsize=(8, 4))
+    fig1, axes1 = plt.subplots(3, 5, figsize=(8, 4))
     grayscale = rgb2gray(picture)
 
     thresh = sk.filters.threshold_otsu(grayscale)
     bw = morphology.closing(grayscale > thresh, morphology.square(5))
     ax = axes1.ravel()
 
-    i = 0
-    ax[i].imshow(picture)
-    ax[i].set_title("Original")
-
-    i = i + 1
-    ax[i].imshow(grayscale, cmap=plt.cm.gray)
-    ax[i].set_title("Gray")
-
-    i = i + 1
+    # i = 0
+    # ax[i].imshow(picture)
+    # ax[i].set_title("Original")
+    #
+    # i = i + 1
+    # ax[i].imshow(grayscale, cmap=plt.cm.gray)
+    # ax[i].set_title("Gray")
+    #
+    # i = i + 1
 
     cleared = segmentation.clear_border(bw)
 
-    print(picture)
-    ax[i].imshow(cleared, cmap=plt.cm.gray)
-    ax[i].set_title("Cleared")
+    label = sk.measure.label(cleared)
+
+    rp = sk.measure.regionprops(label)
+    l2rgb = sk.color.label2rgb(label)
+
+    ax[14].imshow(l2rgb)
+    # ax[i].imshow(cleared, cmap=plt.cm.gray)
+    # ax[i].set_title("Cleared")
+
+    # i = i + 1
+
+    for x in range(len(rp)):
+        print("AREA = " + str(rp[x].area))
+        print("CENTROID = " + str(rp[x].centroid))
+
+        ax[x].imshow(rp[x].image)
+
+    # ax[i].imshow(rp[10].image)
+    # ax[i].set_title("Labels")
 
     fig1.tight_layout()
 
