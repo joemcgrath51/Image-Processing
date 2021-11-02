37fbe5bfdbd74f75b1cbdb806b860b0490773488
Joe McGrath <joseph.mcgrath@techworks.ie>
2021-11-02 14:55:25 +0000

    commit with smallest scissor and random noise added

--- part_2.py
+++ part_2.py
@@ -2,92 +2,33 @@ import skimage.util
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage import data, transform, filters, util, restoration, segmentation, morphology, metrics
+from skimage import filters, segmentation, morphology, measure
 from skimage.color import rgb2gray
 
 import skimage as sk
 if __name__ == '__main__':
     picture = np.array(Image.open("scissors_col_2.jpg"))
 
-    fig1, axes1 = plt.subplots(7, 4, figsize=(30, 20))
     grayscale = rgb2gray(picture)
+
+    grayscale = sk.util.random_noise(grayscale, 'gaussian', mean=0.0, var=0.01)
 
     thresh = sk.filters.threshold_otsu(grayscale)
 
     bw = morphology.closing(grayscale > thresh, morphology.square(5))
-    ax = axes1.ravel()
-
-    # i = 0
-    # ax[i].imshow(picture)
-    # ax[i].set_title("Original")
-    #
-    # i = i + 1
-    # ax[i].imshow(grayscale, cmap=plt.cm.gray)
-    # ax[i].set_title("Gray")
-    #
-    # i = i + 1
 
     cleared = segmentation.clear_border(bw)
 
     label = sk.measure.label(cleared)
 
-    rp = sk.measure.regionprops(label)
+    applied_props = sk.measure.regionprops(label)
     l2rgb = sk.color.label2rgb(label)
 
-    #ax[14].imshow(l2rgb)
-    # ax[i].imshow(cleared, cmap=plt.cm.gray)
-    # ax[i].set_title("Cleared")
+    for x in applied_props:
+        applied_props.sort(key=lambda x: x.euler_number,reverse=False)
+        if x.euler_number <= -1:
+            applied_props.sort(key=lambda x: x.area, reverse=False)
 
-    # i = i + 1
-
-    b = []
-    e_1 = rp[1].euler_number
-    e_2 = rp[9].euler_number
-    t = []
-
-    for x in range(len(rp)):
-        print("AREA = " + str(rp[x].area))
-
-        print("CENTROID = " + str(rp[x].centroid))
-
-        print("Euler Number = " + str(rp[x].euler_number))
-
-        print("Solidity = " + str(rp[x].solidity))
-
-        print("Perimeter = " + str(rp[x].perimeter))
-
-        t.append(transform.resize(rp[x].image, [255,255]))
-        b.append(skimage.img_as_float(t[x]))
-
-        #ax[x].imshow(rp[x], cmap=plt.cm.gray)
-        #ax[x].imshow(b[x])
-
-        #print(b[0])
-    images = []
-    count = 0
-    for x in range(len(b)):
-        for y in range(x+1,len(b)):
-            mse = skimage.metrics.mean_squared_error(b[x], b[y])
-            #if rp[x].euler_number == rp[y].euler_number: # and (0.2 < mse < 0.25):
-
-            ssim = metrics.structural_similarity(t[x], t[y], data_range=t[y].max() - t[y].min())
-            # print(rp[x].euler_number)
-            # print(rp[y].euler_number)
-            # count = count + 1
-            # print(count)
-            if ssim > 0.5 and (rp[x].area/rp[y].area) < 0.3 :
-                #print("SSIM = ", ssim)
-                print("AREA = ", rp[x].area/rp[y].area)
-                images.append(t[x])
-                images.append(t[y])
-    print(len(images))
-    for x in range(len(images)):
-        ax[x].imshow(images[x])
-
-
-
-    # ax[i].set_title("Labels")
-
-    fig1.tight_layout()
-
+    plt.imshow(applied_props[0].image)
+    plt.title("Area = " + str(applied_props[0].area) + " Centroid = " + str(applied_props[0].centroid))
     plt.show()
